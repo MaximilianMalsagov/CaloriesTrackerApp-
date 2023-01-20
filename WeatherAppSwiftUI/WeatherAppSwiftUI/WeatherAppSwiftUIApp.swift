5f89065b1d28218f9a045d693e5f0c39d03ca4cf
Maximilian <117158527+MaximilianMalsagov@users.noreply.github.com>
2023-01-20 14:12:53 +0300

    Add files via upload
    Weather App(with the use of API and Swift UI)

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Maximilian on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
